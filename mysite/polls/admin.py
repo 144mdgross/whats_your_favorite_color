89211df832a61ba97e9ed5c84a3ebfb3fc70d419
144mdgross <144.mdgross@gmail.com>
2017-04-27 19:21:08 -0600

    admin crud improved. user view and voting functional

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+from .models import Choice, Question
+
+# no fieldsets
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+#
+# admin.site.register(Question, QuestionAdmin)
+
+# This line makes the form a long list
+# class ChoiceInline(admin.StackedInline):
+
+#This line makes the talbe inline and easier to read
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+# removing this line to give the option to add choices while makeing a quesiton
+# admin.site.register(Choice)
+
+
